4e4982cfc5379f873954b84c034174bb95fc7b2f
Edigar Maia <edigarmaia@gmail.com>
2023-11-08 22:30:53 -0300

    Criado classe e metodos dos usuarios

---
+++ Projeto Sempre Bela/Mecanismo/Dao/DaoUsuarios/Usuario.cs
@@ -0,0 +1,88 @@
+﻿using Mecanismo.Dao.DaoEnderecos;
+using System.Data.SqlClient;
+
+namespace Mecanismo.Dao.DaoUsuarios
+{
+    public class Usuario
+    {
+        private int idUsuario;
+        private string nome;
+        private string telefone;
+        private string email;
+        private string senha;
+        private string cpf;
+        private int tipo;
+        private Endereco endereco;
+
+        public int IdUsuario { get => idUsuario; set => idUsuario = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+        public string Email { get => email; set => email = value; }
+        public string Senha { get => senha; set => senha = value; }
+        public int Tipo { get => tipo; set => tipo = value; }
+        public string Cpf { get => cpf; set => cpf = value; }
+        public Endereco Endereco { get => endereco; set => endereco = value; }
+
+
+        public static Usuario MapCliente(SqlDataReader leitor)
+        {
+            return new Usuario(
+                idUsuario: (int)leitor["idUsuario"],
+                nome: leitor["nome"].ToString(),
+                telefone: leitor["telefone"].ToString(),
+                cpf: leitor["cpf"].ToString(),
+                tipo: 1);
+        }
+
+        public static Usuario MapManicure(SqlDataReader leitor)
+        {
+            return new Usuario(
+                idUsuario: (int)leitor["idUsuario"],
+                nome: leitor["nome"].ToString(),
+                telefone: leitor["telefone"].ToString(),
+                cpf: leitor["cpf"].ToString(),
+                tipo: 2);
+        }
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(int idUsuario, string nome, int tipo)
+        {
+            IdUsuario = idUsuario;
+            Nome = nome;
+            Tipo = tipo;
+        }
+
+        public Usuario(int idUsuario, string nome, string telefone, string cpf, int tipo)
+        {
+            IdUsuario = idUsuario;
+            Nome = nome;
+            Telefone = telefone;
+            Cpf = cpf;
+            Tipo = tipo;
+        }
+        
+
+        public Usuario(string nome, string telefone, string email, string senha, int tipo, Endereco endereco)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Email = email;
+            Senha = senha;
+            Tipo = tipo;
+            Endereco = endereco;
+        }
+        public Usuario(string nome, string telefone, string cpf,string email, string senha, int tipo)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Cpf = cpf;
+            Email = email;
+            Senha = senha;
+            Tipo = tipo;
+        }
+    }
+}
---
+++ Projeto Sempre Bela/Mecanismo/Dao/DaoUsuarios/UsuariosDao.cs
@@ -0,0 +1,115 @@
+﻿using SempreBela.DaoClientes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Mecanismo.Dao.DaoUsuarios
+{
+    public class UsuariosDao
+    {
+        // Método que valida o login
+        public static Usuario ValidarLogin(string email, string senha)
+        {
+            try
+            {
+                // Recuperando nome e tipo de usuário para manipular em outras partes do sistema.
+                string comandoSql = "SELECT idUsuario, nome, idTipoUsuario FROM usuarios WHERE email = @Email AND senha = @Senha";
+                SqlCommand comando = new SqlCommand(comandoSql, Conexao.GetConexao());
+
+                comando.Parameters.AddWithValue("@Email", email);
+                comando.Parameters.AddWithValue("@Senha", senha);
+
+                var leitor = comando.ExecuteReader();
+
+                while (leitor.Read())
+                    //Utilizando construtor com parâmetro nomeado
+                    return new Usuario(idUsuario: (int)leitor["idUsuario"], nome: leitor["nome"].ToString(), tipo: (int)leitor["idTipoUsuario"]);
+
+                return null;
+            }
+            catch (Exception ex)
+            {
+                //tratar erros aqui
+                return null;
+            }
+
+        }
+
+        // Método que lista manicures
+        public static List<Usuario> ListarManicures()
+        {
+            try
+            {
+                List<Usuario> manicures = new List<Usuario>();
+                string comandoSql = "SELECT idUsuario, nome, telefone, cpf FROM usuarios WHERE idTipoUsuario = 2";
+                SqlCommand comando = new SqlCommand(comandoSql, Conexao.GetConexao());
+
+                var leitor = comando.ExecuteReader();
+
+                while (leitor.Read())
+                    manicures.Add(Usuario.MapManicure(leitor));
+
+                return manicures;
+            }
+            catch (Exception ex)
+            {
+                //tratar erros aqui
+                return null;
+            }
+
+        }
+
+        // Método que insere um usuario
+        public static int InserirUsuario(Usuario usuario)
+        {
+            // Comando Sql
+            string comandoSql = @"INSERT INTO usuarios (nome, telefone, cpf, email, senha, idTipoUsuario) 
+                                  VALUES (@nome, @telefone, @cpf, @email, @senha, @tipo); 
+                                  SELECT SCOPE_IDENTITY();";
+
+            // Comando
+            SqlCommand comando = new SqlCommand(comandoSql, Conexao.GetConexao());
+
+            // Configuração dos parâmetros do comando SQL
+            SqlParameter nomeUsuario = new SqlParameter("@nome", System.Data.SqlDbType.Text, 50);
+            SqlParameter telefoneUsuario = new SqlParameter("@telefone", System.Data.SqlDbType.Text, 20);
+            SqlParameter cpfUsuario = new SqlParameter("@cpf", System.Data.SqlDbType.Text, 11);
+            SqlParameter emailUsuario = new SqlParameter("@email", System.Data.SqlDbType.Text, 50);
+            SqlParameter senhaUsuario = new SqlParameter("@senha", System.Data.SqlDbType.Text, 20);
+            SqlParameter tipoUsuario = new SqlParameter("@tipo", System.Data.SqlDbType.Int);
+
+            // Atribuição dos valores aos parâmetros do comando SQL
+            nomeUsuario.Value = usuario.Nome;
+            telefoneUsuario.Value = usuario.Telefone;
+            cpfUsuario.Value = usuario.Cpf;
+            emailUsuario.Value = usuario.Email;
+            senhaUsuario.Value = usuario.Senha;
+            tipoUsuario.Value = usuario.Tipo;
+
+            // Adição dos parâmetros ao comando SQL
+            comando.Parameters.Add(nomeUsuario);
+            comando.Parameters.Add(telefoneUsuario);
+            comando.Parameters.Add(cpfUsuario);
+            comando.Parameters.Add(emailUsuario);
+            comando.Parameters.Add(senhaUsuario);
+            comando.Parameters.Add(tipoUsuario);
+
+
+            // Compila a instrução e a submete ao banco de dados
+            comando.Prepare();
+            int idUsuario = Convert.ToInt32(comando.ExecuteScalar());
+
+            comando.Dispose();
+
+            return idUsuario;
+
+        }
+
+    }
+}
+    
+
+
+
+
+
